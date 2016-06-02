6db9ed18cc496c4443ab5a920a08fe161fc0e376
beta <rajudawadinp@gmail.com>
2016-06-02 21:16:06 +0545

    weather subs completed

--- js-basics/weather_subs.js
+++ js-basics/weather_subs.js
@@ -1,10 +1,11 @@
-var input = '32.6 31.2 35.2 37.4 44.9 42.1 44.1';
+var input = "32.6 31.2 35.2 37.4 44.9 42.1 44.1";
 
 tempArray = new Array();
-tempArray = input.split(" ");
-console.log(tempArray);
-
-for(i=0; i<tempArray.length-1; i++){
-	tempArray[i+1] = ((tempArray[i]+tempArray[i+2])/2);
+tempArray = input.split(" ").map(Number);
+tempArray_new = input.split(" ").map(Number);
+for(i=1; i<tempArray.length-1; i++){
+	
+	tempArray_new[i] = (tempArray[i]+tempArray[i-1]+ tempArray[i+1])/3;	
+	
 }
-console.log(tempArray);
+console.log(tempArray_new.join(" "));
